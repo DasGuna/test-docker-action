236e3ccc183db03dcb11ae98ae35c91daf3b9190
Dasun <dasun.gunasinghe@qut.edu.au>
2022-02-01 11:21:17 +1000

    self-testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 apt-get -y update 
 apt-get install -y python3-pip python3-dev python3-bloom python3-stdeb dh-make git wget fakeroot
 cd /usr/local/bin && ln -s /usr/bin/python3 python && pip3 install --upgrade pip
-cd ~ && ls
+cd /github/workspace
 
 echo "Hello Test - input ROS Distro is [$1]"
-time=$(date)
-echo "::set-output name=time::$time"
-echo "source ros installation"
+echo "Source ROS Installation"
 source /opt/ros/$1/setup.bash
-echo "check current directory content"
+echo "Checking current directory path"
+pwd
+echo "Checking current directory content"
 ls -la
-echo "current architecture"
+echo "Current architecture check"
 uname -m
 echo "clone release tools action from QCR repos"
 git clone https://github.com/qcr/release-tools-ros.git
@@ -21,3 +21,7 @@ cd release-tools-ros
 ln -s ../ws/src src
 echo "running release script"
 ./release
+
+# Set the outputs
+time=$(date)
+echo "::set-output name=time::$time"
